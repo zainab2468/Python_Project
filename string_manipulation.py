91dfe0374ca2e03be70ff8a4a518465092f7dc7f
Zainab <zainab.t.ojeleye@gmial.com>
2024-11-17 17:12:36 +0100

    added feature

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,1 +1,20 @@
-hr
+#Reverse a string
+name = "Rasford Cliton "
+reverse_name = name[::-1]
+print(reverse_name)
+
+
+
+#counting number of vowels in a string 
+word = "Citizenship Education"
+vowels = "aeiouAEIOU" 
+
+i = word.count(vowels)
+print(i)
+
+
+
+#Replacing a word with another word
+word = "Hello World"
+new = word.replace("Hello", "Happy")
+print(new)
